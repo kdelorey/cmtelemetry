8a81744c857edaebf597fefa84dc8834e8b53b70
Kevin DeLorey <2295721+kdelorey@users.noreply.github.com>
2018-03-09 16:08:26 -0600

    Initial commit of the telemetry.go file. Contains basic support for udp ports

---
+++ telemetry.go
@@ -0,0 +1,48 @@
+package dirt
+
+import "net"
+
+// Telemetry is the basic
+type Telemetry struct {
+	udp    *net.UDPConn
+	buffer []byte
+}
+
+// StartDefaultTelemetry will open the default udp port and begin processing
+// telemetry from Codemasters' games.
+func StartDefaultTelemetry() (*Telemetry, error) {
+	return StartTelemetry(":20777")
+}
+
+// StartTelemetry will open a udp port up at the specified location and begin
+// processing telemetry from Codemasters' games.
+func StartTelemetry(address string) (telemetry *Telemetry, err error) {
+	addr, err := net.ResolveUDPAddr("udp", address)
+
+	if err != nil {
+		return
+	}
+
+	udp, err := net.ListenUDP("udp", addr)
+
+	if err != nil {
+		return
+	}
+
+	telemetry = &Telemetry{udp: udp, buffer: make([]byte, 264)}
+
+	//go telemetry.telemetryRotine()
+
+	return
+}
+
+func (telemetry *Telemetry) telemetryRotine() {
+	for {
+		telemetry.udp.ReadFromUDP(telemetry.buffer)
+	}
+}
+
+// Close will stop the connection.
+func (telemetry *Telemetry) Close() {
+	telemetry.Close()
+}
