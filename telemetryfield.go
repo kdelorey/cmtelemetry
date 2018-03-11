52f2cd7edcc096fc484ad0f4a7302af4cabb9a35
Kevin DeLorey <2295721+kdelorey@users.noreply.github.com>
2018-03-11 09:11:03 -0500

    Added the rest of fields.

--- telemetryfield.go
+++ telemetryfield.go
@@ -96,4 +96,172 @@ const (
 	// SuspensionVelocityFrontRight is the vertical velocity of the wheel from the
 	// rest (in vehicle space), measured in meteres per second (m/s).
 	SuspensionVelocityFrontRight
+
+	// WheelPatchSpeedBackLeft is the speed of the contact patch of the back left
+	// wheel in meters per second (m/s).
+	WheelPatchSpeedBackLeft
+
+	// WheelPatchSpeedBackRight is the speed of the contact patch of the back
+	// right wheel in meters per second (m/s).
+	WheelPatchSpeedBackRight
+
+	// WheelPatchSpeedFrontLeft is the speed of the contact patch of the front
+	// left wheel in meters per second (m/s).
+	WheelPatchSpeedFrontLeft
+
+	// WheelPatchSpeedFrontRight is the speed of the contact patch of the front
+	// right wheel in meters per second (m/s).
+	WheelPatchSpeedFrontRight
+
+	// ThrottleInput is the level of throttle input applied through the controller.
+	// A normalized value between 0 and 1.
+	ThrottleInput
+
+	// SteeringInput the level of steering input applied through the controller. A
+	// value between -1 and 1.
+	SteeringInput
+
+	// BrakeInput is the level of brake input applied through the controller. A
+	// normalized value between 0 and 1.
+	BrakeInput
+
+	// ClutchInput is the level of clutch input applied through the controller. A
+	// normalized value between 0 and 1.
+	ClutchInput
+
+	// Gear is the current gear number. 0 for neutral, -1 for reverse.
+	Gear
+
+	// GForceLateral is the G-Force in the lateral direction.
+	GForceLateral
+
+	// GForceLongitudinal is the G-Force in the longitudinal direction.
+	GForceLongitudinal
+
+	// Lap is the current lap number.
+	Lap
+
+	// EngineRate is the engine rotations in radians per second.
+	EngineRate
+
+	// NativeSliSupport (undocumented, the following is speculation) is native
+	// support for shift light indicators.
+	NativeSliSupport
+
+	// RacePosition is the current position within the race.
+	RacePosition
+
+	// KersLevel (undocumented, the following is speculation) is the level of
+	// of the kinetic energy recovery system.
+	//
+	// Not supported in DiRT 4
+	KersLevel
+
+	// KersLevelMax (undocumented, the following is speculation) is the maximum
+	// level of of the kinetic energy recovery system.
+	//
+	// Not supported in DiRT 4
+	KersLevelMax
+
+	// DRS (undocumented, the following is speculation) involves the drag reduction
+	// system
+	//
+	// Not supported in DiRT 4
+	DRS
+
+	// TractionControl (undocumented, the following is speculation) is whether
+	// traction control is engaged.
+	//
+	// Not supported in DiRT 4
+	TractionControl
+
+	// ABS (undocumented, the following is speculation) is whether ABS is engaged.
+	//
+	// Not supported in DiRT 4
+	ABS
+
+	// FuelInTank (undocumented, the following is speculation) is the amount of
+	// fuel in the tank.
+	//
+	// Not supported in DiRT 4
+	FuelInTank
+
+	// FuelCapacity (undocumented, the following is speculation) is maximum
+	// capacity of fuel.
+	//
+	// Not supported in DiRT 4
+	FuelCapacity
+
+	// InPits (undocumented, the following is speculation) is whether or not the
+	// car is in the pit area.
+	//
+	// Not supported in DiRT 4
+	InPits
+
+	// RaceSector is the number of splits in the lap.
+	RaceSector
+
+	// SectorTime1 is the time of the first split.
+	SectorTime1
+
+	// SectorTime2 is the time of the second split.
+	SectorTime2
+
+	// BrakeTempBackLeft is the temperature of the back left brake in °C.
+	BrakeTempBackLeft
+
+	// BrakeTempBackRight is the temperature of the back right brake in °C.
+	BrakeTempBackRight
+
+	// BrakeTempFrontLeft is the temperature of the front left brake in °C.
+	BrakeTempFrontLeft
+
+	// BrakeTempFrontRight is the temperature of the front left brake in °C.
+	BrakeTempFrontRight
+
+	// TirePressureBackLeft (undocumented, the following is speculation) is the
+	// current pressure of the back left tire.
+	//
+	// Not supported in DiRT 4
+	TirePressureBackLeft
+
+	// TirePressureBackRight (undocumented, the following is speculation) is the
+	// current pressure of the back right tire.
+	//
+	// Not supported in DiRT 4
+	TirePressureBackRight
+
+	// TirePressureFrontLeft (undocumented, the following is speculation) is the
+	// current pressure of the front left tire.
+	//
+	// Not supported in DiRT 4
+	TirePressureFrontLeft
+
+	// TirePressureFrontRight (undocumented, the following is speculation) is the
+	// current pressure of the front right tire.
+	//
+	// Not supported in DiRT 4
+	TirePressureFrontRight
+
+	// LapsCompleted (undocumented, the following is speculation) is the number
+	// of laps completed.
+	LapsCompleted
+
+	// TotalLaps is the total number of laps in the race.
+	TotalLaps
+
+	// TrackLength is the total length of the track in meters.
+	TrackLength
+
+	// LastLapTime is the time of the last lap, in seconds.
+	LastLapTime
+
+	// MaxRpm is the maximum engine rate, in radians per second.
+	MaxRpm
+
+	// IdleRpm is the idle rate of the engine in radians per second.
+	IdleRpm
+
+	// MaxGears is the number of forward gears available in the vehicle.
+	MaxGears
 )
